87cc3c4418ab9e9ce00f6dd52d09dcac191ffcb2
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-07-27 22:35:27 +0100

    feature: signup model

--- model/signup.model.js
+++ model/signup.model.js
@@ -1,54 +1,70 @@
-const express = require("express");
-const { Schema } = require("mongoose");
+const {
+    Schema
+} = require("mongoose");
+const {
+    uuid
+} = require("uuidv4");
 
 const validateEmail = function (email) {
-  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
 };
 
-const signUpSchema = new Schema(
-  {
+const signUpSchema = new Schema({
+    _id: {
+        type: mongooose.Schema.Types.ObjectId,
+        required: true,
+        default: uuid().split("-").join(""),
+    },
     fullName: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 3,
-      maxlength: 50,
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 50,
     },
     email: {
-      required: true,
-      trim: true,
-      lowercase: true,
-      unique: true,
-      type: String,
-      validate: [validateEmail, "Please fill a valid email address"],
-      match: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        type: String,
+        validate: [validateEmail, "Please fill a valid email address"],
+        match: /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/,
     },
     gender: {
-      type: String,
-      required: true,
-      enum: ["male", "female"],
-      trim: true,
+        type: String,
+        required: true,
+        enum: ["male", "female"],
+        trim: true,
     },
     stateOfResidence: {
-      type: String,
-      required: true,
-      trim: true,
-      default: "Lagos",
+        type: String,
+        required: true,
+        trim: true,
+        default: "Lagos",
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        maxlength: 50,
     },
     signForNewsLetter: {
-      type: Boolean,
-      required: true,
-      default: false,
+        type: Boolean,
+        required: true,
+        default: false,
     },
     isLoggedIn: {
-      type: Boolean,
-      require: true,
-      default: false,
+        type: Boolean,
+        require: true,
+        default: false,
     },
-  },
-  { timestamps: true }
-);
+    
+}, {
+    timestamps: true
+});
 
 const SignUp = mongoose.model("SignUp", signUpSchema);
 module.exports = SignUp;
