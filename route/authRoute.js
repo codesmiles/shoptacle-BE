b8d03d25f17ae7e86132423a92ab069b8ce65052
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-07-28 12:29:37 +0100

    feature/codesmiles/Set up the database

--- route/authRoute.js
+++ route/authRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const signupController = require("../controller/signupController");
 
 // signup
+router.post("/sign-up", signupController.signup);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+// jshint esversion: 6
+require("dotenv").config();
 const express = require('express');
-const app = express();
 const authRoute = require("./route/authRoute");
+const mongoose = require("mongoose"); //import mongoose
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/api', authRoute);
+
+
+const url = `mongodb://localhost:27017/shoptacleDB`;
+
+// connect to mongoose
+mongoose.connect(url, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Connected to MongoDB`);
+  }
+});
+
 
 
 
