061c57c7a1139d5cd75e85558eebcd6b6b57f56a
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-08-04 00:18:39 +0100

    worked on initializing paystack on the backend

--- controller/paymentControllers/paystackController.js
+++ controller/paymentControllers/paystackController.js
@@ -1,16 +1,46 @@
 const request = require("../../config/paystackConfig");
+const https = require("https");
 
 // INITIALIZE PAYSTACK
 module.exports.initialize=async(req,res)=>{
 let{amount,email}=await req.body;
-// GO
 const params = JSON.stringify({
-    email,
-    amount,
+  email,
+  amount
+});
+
+const options = {
+  hostname: "api.paystack.co",
+  port: 443,
+  path: "/transaction/initialize",
+  method: "POST",
+  headers: {
+    "Authorization":`Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`,
+    "Content-Type": "application/json",
+  },
+};
+
+const request = https
+  .request(options, (response) => {
+    let data = "";
+
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    response.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (error) => {
+    console.error(error);
   });
 
+  
+  
   request.write(params);
   request.end();
+  // res.send(request.end().outputData);
 
 }
 
