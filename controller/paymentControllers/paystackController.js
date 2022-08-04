6eabba7ccb20e3a2a7ca084dbaf0c3b21f9dfa3c
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-08-04 22:59:43 +0100

    update

--- controller/paymentControllers/paystackController.js
+++ controller/paymentControllers/paystackController.js
@@ -6,74 +6,71 @@ const options = {
   path: "/transaction/initialize",
   method: "POST",
   headers: {
-    "Authorization":`Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`,
+    Authorization: `Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`,
     "Content-Type": "application/json",
   },
 };
 
-
-
 // INITIALIZE PAYSTACK
-module.exports.initialize=async(req,res)=>{
-let{amount,email}=await req.body;
-const params = JSON.stringify({
-  email,
-  amount
-});
-
-// const options = {
-//   hostname: "api.paystack.co",
-//   port: 443,
-//   path: "/transaction/initialize",
-//   method: "POST",
-//   headers: {
-//     "Authorization":`Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`,
-//     "Content-Type": "application/json",
-//   },
-// };
-
-const request = https
-  .request(options, (response) => {
-    let data = "";
-
-    response.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    response.on("end", () => {
-      console.log(JSON.parse(data));
-    });
-  })
-  .on("error", (error) => {
-    console.error(error);
+module.exports.initialize = async (req, res) => {
+  let { amount, email } = await req.body;
+  const params = JSON.stringify({
+    email,
+    amount,
   });
 
-  
-  
+  // const options = {
+  //   hostname: "api.paystack.co",
+  //   port: 443,
+  //   path: "/transaction/initialize",
+  //   method: "POST",
+  //   headers: {
+  //     "Authorization":`Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`,
+  //     "Content-Type": "application/json",
+  //   },
+  // };
+
+  const request = https
+    .request(options, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        console.log(JSON.parse(data));
+      });
+    })
+    .on("error", (error) => {
+      console.error(error);
+    });
+
   request.write(params);
   request.end();
   // res.send(request.end().outputData); // figure out how to store the reference of the transaction to the database
+};
 
-}
-
-module.exports.verify=async(req,res)=>{
-  let reference= await req.params.reference;
+module.exports.verify = async (req, res) => {
+  let reference = await req.params.reference;
 
   options.path = `/transaction/verify/${reference}`; //reference from initialize
 
-  console.log(options);
-  return 0
-  https.request(options, res => {
-    let data = ''
-  
-    res.on('data', (chunk) => {
-      data += chunk
-    });
-  
-    res.on('end', () => {
-      console.log(JSON.parse(data))
+  // console.log(options);
+
+  https
+    .request(options, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        console.log(JSON.parse(data));
+      });
     })
-  }).on('error', error => {
-    console.error(error)
-  })
-}
+    .on("error", (error) => {
+      console.error(error);
+    });
+};
